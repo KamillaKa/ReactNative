5644fac303f9c8e33b0ee52d93e169ad76283e8e
Kamilla <KamillaKa@users.noreply.github.com>
2024-02-28 13:34:41 +0200

    native-components

--- App.tsx
+++ App.tsx
@@ -1,20 +1,23 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {StatusBar} from 'expo-status-bar';
+import {Platform, SafeAreaView, StyleSheet, Text} from 'react-native';
+import Home from './src/views/Home';
 
-export default function App() {
+const App = () => {
+  console.log('moro');
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+    <SafeAreaView style={styles.container}>
+      <Home />
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
-}
+};
+
+export default App;
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: Platform.OS === 'android' ? 30 : 0,
   },
 });
