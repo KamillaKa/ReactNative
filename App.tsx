97e2b6e38873ab90a85f1702029f0324291a12ad
Kamilla <KamillaKa@users.noreply.github.com>
2024-03-04 12:36:52 +0200

    login

--- App.tsx
+++ App.tsx
@@ -1,13 +1,16 @@
 import {StatusBar} from 'expo-status-bar';
-import {SafeAreaProvider} from 'react-native-safe-area-context';
 import Navigator from './src/navigators/Navigator';
+import {UserProvider} from './src/contexts/UserContext';
+import {UpdateProvider} from './src/contexts/UpdateContext';
 
 const App = () => {
   return (
-    <SafeAreaProvider>
-      <Navigator />
-      <StatusBar style="auto" />
-    </SafeAreaProvider>
+    <UserProvider>
+      <UpdateProvider>
+        <Navigator />
+        <StatusBar style="auto" />
+      </UpdateProvider>
+    </UserProvider>
   );
 };
 
---
+++ README.md
@@ -0,0 +1,1 @@
+![Profile](./screenshots/Screenshot_2024-03-04-12-34-08-26_f73b71075b1de7323614b647fe394240.jpg)
